efdfa9be0a1fb3b2edc5f9693f356b7ea10b889b
Ilham Akbar <31720600+ilham301098@users.noreply.github.com>
2023-09-02 09:55:52 +0700

    Init project and api key mechanism

---
+++ app/Http/Middleware/ApiKeyMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\DB;
+use Closure;
+
+class ApiKeyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $validApi = DB::table('client_api')->where('api_key', $request->key)->first();
+        if($validApi){
+            if($validApi->status=='deactive'){
+                return response(['message'=>'Unauthorized Access'], 400);
+            } 
+            return $next($request);
+        }
+
+        return response(['message'=>'Unauthorized Access'], 400);
+    }
+}
